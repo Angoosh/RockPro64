0ec2e24eab9d4a337825b5e0360ec9b41e25f9e4
Angoosh <kaplanantonin@gmail.com>
2019-01-05 12:07:58 +0100

    Update resolution.sh

--- scripts/resolution.sh
+++ scripts/resolution.sh
@@ -1,1 +1,5 @@
-fbset -xres 800 -yres 480
+sudo apt install fbset -y
+sudo echo fbset -xres 800 -yres 480 > /etc/init.d/resolution.sh
+sudo chmod +x /etc/init.d/resolution.sh
+sudo update-rc.d resolution.sh defaults
+rm resolution.sh
